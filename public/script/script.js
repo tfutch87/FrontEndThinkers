881b8610c1a8eb27ab0ae6efc3d31a29df578a91
Tyren Futch <tfutchtf.43@gmail.com>
2023-01-23 13:10:47 -0500

    added brand svgs, made mobile friendly

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,10 @@
-$(window).scroll(function () {
-    var sticky = $('.sticky'),
-        scroll = $(window).scrollTop();
+// $(window).scroll(function () {
+//     var sticky = $('.sticky'),
+//         scroll = $(window).scrollTop();
 
-    if (scroll >= 100) sticky.addClass('fixed');
-    else sticky.removeClass('fixed');
-});
+//     if (scroll >= 100) sticky.addClass('fixed');
+//     else sticky.removeClass('fixed');
+// });
 
 // nav link active
 
@@ -22,3 +22,24 @@ function removeClass(elements) {
         element.classList.remove('active');
     })
 }
+
+const menu_btn = document.querySelector('.hamburger-menu');
+const mobile_nav = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', (e) => {
+
+    menu_btn.classList.toggle('is-active');
+    mobile_nav.classList.toggle('is-active')
+
+    if (e.target.classList.contains('is-active')) {
+
+        document.body.classList.add('body-no-scroll');
+
+    } else {
+
+        document.body.classList.remove('body-no-scroll');
+
+    }
+
+
+})
